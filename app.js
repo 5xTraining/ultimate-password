6108985d532dc63d3cac9c55e828cb1fc44767c4
krystal <krystal93002232@gmail.com>
2023-06-07 14:51:24 +0800

    feature: add show answer

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
+const lotteryNumber = document.querySelector(".lottery-number");
+const lotteryButton = document.querySelector(".lottery-button");
+const currentMinNumber = document.querySelector(".current-min-number");
+const currentMaxNumber = document.querySelector(".current-max-number");
 const guessNumber = document.querySelector(".guess-number");
 const submitButton = document.querySelector(".submit-button");
 const hint = document.querySelector(".hint");
-const currentMinNumber = document.querySelector(".current-min-number");
-const currentMaxNumber = document.querySelector(".current-max-number");
-const lotteryNumber = document.querySelector(".lottery-number");
-const lotteryButton = document.querySelector(".lottery-button");
 const restartButton = document.querySelector(".restart-button");
 let randomInt = getRandomInt(0, 100);
 
-restartButton.addEventListener("click", function () {
-  // 重新設定遊戲狀態，例如重新生成隨機數、清空輸入框、重置範圍等等
-  randomInt = getRandomInt(0, 100);
-  guessNumber.value = "";
-  hint.textContent = "";
-  currentMinNumber.textContent = "0";
-  currentMaxNumber.textContent = "100";
-});
-
-lotteryButton.addEventListener("click", function () {
-  lotteryNumber.textContent = getRandomInt(0, 100);
-});
-
 // 輸入最大數跟最小數，計算隨機數
 function getRandomInt(min, max) {
-  min = Math.ceil(min); // 回傳大於或等於 min 的最小整數
-  max = Math.floor(max); // 回傳小於或等於 max 的最大整數
+  min = Math.ceil(min); // 回傳大於或等於 min 的最小整
+  max = Math.floor(max); // 回傳小於或等於 max 的最大整數   －
 
   return Math.floor(Math.random() * (max - min) + min);
   // 先用 random 算出兩數的隨機數，再用 floor 將隨機數去小數點
 }
 
-// click 事件處理函數
+lotteryButton.addEventListener("click", function () {
+  lotteryNumber.textContent = getRandomInt(0, 100);
+});
+
 function handleSubmit() {
   if (Number(guessNumber.value) == randomInt) {
-    hint.textContent = "恭喜猜中！";
+    hint.textContent = `恭喜猜中！答案為：${guessNumber.value}`;
   } else if (
     Number(guessNumber.value) > randomInt &&
     Number(guessNumber.value) < currentMaxNumber.textContent
@@ -61,4 +51,13 @@ guessNumber.addEventListener("keydown", function (event) {
     // 如果按下的是 Enter 鍵
     handleSubmit();
   }
+});
+
+restartButton.addEventListener("click", function () {
+  // 重新設定遊戲狀態，例如重新生成隨機數、清空輸入框、重置範圍等等
+  randomInt = getRandomInt(0, 100);
+  guessNumber.value = "";
+  hint.textContent = "";
+  currentMinNumber.textContent = "0";
+  currentMaxNumber.textContent = "100";
 });
